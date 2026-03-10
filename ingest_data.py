00c2c376296aa96738317b4439daa4da8353fd24
vinceTLL <vince.esti@gmail.com>
2026-03-10 20:18:07 +0200

    refactoring script and including dim table

--- ingest_data.py
+++ ingest_data.py
@@ -3,19 +3,37 @@ import duckdb
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 import click
+import pandas as pd
 from pathlib import Path
 import sys
 
 
 
+dtype = {
+  'LocationID': 'int64',
+ 'Borough':"string" ,
+ 'Zone': "string",
+ 'service_zone':"string" }
 
-def get_duckdb_reader(prefix: str, file_name: str, batch_size: int, year: int, month: int):
+
+def read_csv_file(file_name: str, batch_size:int,file_format:str):
+  
+  """ """
+
+  try:
+      url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/{file_name}.{file_format}"
+      iter_data = pd.read_csv(url,dtype=dtype,chunksize=batch_size,iterator=True)
+      return iter_data
+  except Exception as e:
+     print(f"Unbale to read csv file {e}")
+     return
+  
+
+def get_duckdb_reader(file_name: str, batch_size: int, year: int, month: int, file_format:str):
 
     """
     Split parquet data into Batches using duckDB
     
-    :param prefix: location of data
-    :type prefix: str
     :param file_name: name of the file
     :type file_name: str
     :param batch_size: Size of the batch
@@ -31,19 +49,19 @@ def get_duckdb_reader(prefix: str, file_name: str, batch_size: int, year: int, m
 
     con = duckdb.connect()
     con.execute("INSTALL httpfs; LOAD httpfs;")
-    url =f"{prefix}{file_name}_{year}-{month:02d}.parquet"
+    url =f"https://d37ci6vzurychx.cloudfront.net/trip-data/{file_name}_{year}-{month:02d}.{file_format}"
     dr_path = Path(Path.cwd(),"query.sql")
 
-    if Path.exists(dr_path):
+    if dr_path.exists:
 
-        with open("query.sql","r") as q:
-            query = q.read().format(url)
+       query = dr_path.read_text().format(url)
     
     else:
         query = f"SELECT * FROM '{url}'"
 
     try:
         record_batch_reader = con.execute(query).fetch_record_batch(rows_per_batch=batch_size)
+        con.close()
         
         return record_batch_reader
     
@@ -55,18 +73,19 @@ def get_duckdb_reader(prefix: str, file_name: str, batch_size: int, year: int, m
 
 
 @click.command()
+@click.option('--user',default= 'root',help = 'PostgreSQL user')
+@click.option('--password',default= 'root',help = 'PostgreSQL password')
+@click.option('--host',default= 'localhost',help = 'PostgreSQL localhost')
+@click.option('--port',default= 5432,type=int,help = 'PostgreSQL port')
+@click.option('--database',default= 'ny_taxi',help = 'PostgreSQL database')
+@click.option('--target_table',default= 'yellow_taxi_data',help = 'PostgreSQL table')
+@click.option('--file_name',default= 'green_tripdata',help = 'name of file')
+@click.option('--batch_size',default= 10000,type=int,help = 'size of each data batch')
 @click.option('--year',default= 2025,type=int,help = 'Year of data')
 @click.option('--month',default= 11,type=int,help = 'Month of data')
-@click.option('--port',default= 5432,type=int,help = 'PostgreSQL port')
-@click.option('--user',default= 'root',help = 'PostgreSQL user')
-@click.option('--database',default= 'ny_taxi',help = 'PostgreSQL database')
-@click.option('--host',default= 'localhost',help = 'PostgreSQL localhost')
-@click.option('--password',default= 'root',help = 'PostgreSQL password')
-@click.option('--batch_size',default= 10000,type=int,help = 'size of each data batch')
-@click.option('--prefix',default= 'https://d37ci6vzurychx.cloudfront.net/trip-data/',help = 'data prefix')
-@click.option('--file_name',default= 'green_tripdata',help = 'name of file')
-@click.option('--target_table',default= 'yellow_taxi_data',help = 'PostgreSQL table')
-def run_ingestion(user,password,host,port,database,target_table,prefix,file_name,batch_size,year,month):
+@click.option('--file_format',default= 'parquet',help = 'format of source file')
+
+def run_ingestion(user,password,host,port,database,target_table,file_name,batch_size,year,month, file_format):
 
     """
     Ingest data into PostgreSQL database into chunks
@@ -77,7 +96,6 @@ def run_ingestion(user,password,host,port,database,target_table,prefix,file_name
     :param port: Port
     :param database: 
     :param target_table: table to load the data
-    :param prefix: location of data
     :param file_name: name of the file
     :param batch_size: size of the batch
     :param year: year data was collected
@@ -96,11 +114,17 @@ def run_ingestion(user,password,host,port,database,target_table,prefix,file_name
 
     except Exception as e:
         print(f"failed to connect to PostgreSQL: {e}")
+        engine.dispose()
         return 
     
 
+
     # Ingest data to Database
-    batch_reader = get_duckdb_reader(prefix,file_name,batch_size,year,month)
+    if file_format == "csv":
+        batch_reader = read_csv_file(file_name,batch_size,file_format)
+    else:
+        batch_reader = get_duckdb_reader(file_name,batch_size,year,month,file_format)
+
 
     if batch_reader is None:
         print("No data found or error reading stream.")
@@ -109,7 +133,12 @@ def run_ingestion(user,password,host,port,database,target_table,prefix,file_name
     first = True
     try:
         for batch in tqdm(batch_reader):
-            chunk_df = batch.to_pandas()
+
+            try:
+                chunk_df = batch.to_pandas()
+            except AttributeError:
+                chunk_df = pd.DataFrame(batch)
+
 
             if first:
                 chunk_df.to_sql(name = target_table, con = engine, if_exists = "replace")
@@ -121,6 +150,7 @@ def run_ingestion(user,password,host,port,database,target_table,prefix,file_name
         print(f"Error during ingestion {e}")
     
     finally:
+        engine.dispose()
         print("Ingestion job finished.")
 
 
@@ -129,5 +159,5 @@ def run_ingestion(user,password,host,port,database,target_table,prefix,file_name
 #docker run -it --rm -e PGADMIN_DEFAULT_EMAIL=admin@admin.com -e PGADMIN_DEFAULT_PASSWORD="root" -v pgadmin_data:/var/lib/pgadmin -p 8085:80 --network=pg-network --name pgadmin dpage/pgadmin4
 
 
-#docker run -it --network=pg-network taxi_ingest:v001 --user=root --password=root --host=pgdatabase --port=5432 --database=ny_taxi --target_table=yellow_taxi_data --prefix="https://d37ci6vzurychx.cloudfront.net/trip-data/" --file_name=green_tripdata --batch_size=10000 --year=2025 --month=11
+#docker run -it --network=pg-network taxi_ingest:v001 --user=root --password=root --host=pgdatabase --port=5432 --database=ny_taxi --target_table=yellow_taxi_data --file_name=green_tripdata --batch_size=10000 --year=2025 --month=11 --file_format=parquet
 #
